0e534fcc17f7f93ed29431a22dc0f20b4665cf86
smich <tsmisek@seznam.cz>
2023-01-24 11:55:14 +0100

    working

--- 01_create_environment.sh
+++ 01_create_environment.sh
@@ -1,5 +1,3 @@
-# === POSTGRES ===
-
 #!/usr/bin/env bash
 CURRENT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 DB_PATH="$CURRENT_DIR/DB"
@@ -7,37 +5,15 @@ SCRIPT_PATH="$CURRENT_DIR/scripts"
 TMP_PATH="$CURRENT_DIR/tmp"
 WEB_INTERFACE_PATH="$CURRENT_DIR/web"
 CONT_ITER="2"
-PG_PATH="$TEMP_PATH/postgres$CONT_ITER"
-#PG_EXCHANGE="$PG_PATH/exchange"
-PG_IMAGE='postgres:9.6'
 PGPWD='Pa$$w0rd'
-
 docker stop web2epub$CONT_ITER
 docker rm -f web2epub$CONT_ITER
-#rm -rf "$PG_PATH"
-docker run --name web2epub$CONT_ITER --detach -p 5432:543$CONT_ITER -p 5000:5000 -e POSTGRES_PASSWORD=$PGPWD -v  $WEB_INTERFACE_PATH:/app/web -v $TMP_PATH:/tmp:rw -v $SCRIPT_PATH:/app/scripts:ro -v $DB_PATH:/app/DB:ro $PG_IMAGE
-sleep 10 #--add-host=host.docker.internal:host-gateway 
-# cp -r -a "$CURRENT_DIR/data/postgres/db/" "$PG_EXCHANGE"
 
-docker exec web2epub$CONT_ITER /bin/sh -c '/app/DB/restore.ps'
-
-# docker exec pg$CONT_ITER /bin/sh -c 'psql postgres --host=localhost --username=postgres -f /tmp/db/create_user_database.sql'
-# docker exec pg$CONT_ITER /bin/sh -c 'psql postgres --host=localhost --username=postgres -d test.db -f /tmp/db/test.db'
-# docker exec pg$CONT_ITER  /bin/sh -c 'ls /tmp/db'
-
-
-##!/usr/bin/env bash
-#echo "***********************************"
-#dt=$(date '+%d/%m/%Y %H:%M:%S');
-#echo "$dt"
-#echo "***********************************"
-#CURRENT_DIR=$(dirname "$0")
-#TEMP_PATH="$CURRENT_DIR/tmp"
-#INSTANCE=1
-#MSSQL_PWD="Hold-D00r"
-#logs_vol="$TEMP_PATH/tmp"
-#image="mcr.microsoft.com/mssql/server:2022-latest"
-#docker run -d --name mssql-$INSTANCE -p 1433:1433 -e ACCEPT_EULA=Y -e SA_PASSWORD=$MSSQL_PWD -e MSSQL_PID=Developer -v $TEMP_PATH:/tmp/sqlserver/data:ro -v $logs_vol/mssql$INSTANCE:/var/opt/mssql/log:rw --add-host=host.docker.internal:host-gateway $image
-#sleep 10
-
-
+docker build -t web2epub .
+echo "Docker run"
+docker run --name web2epub$CONT_ITER --detach -p 5432:543$CONT_ITER -p 5000:5000 -e POSTGRES_PASSWORD=$PGPWD -v  $WEB_INTERFACE_PATH:/tmp/web -v $TMP_PATH:/tmp/tmp:rw -v $SCRIPT_PATH:/tmp/scripts:ro -v $DB_PATH:/tmp/DB:ro web2epub 
+# docker run --name web2epub$CONT_ITER --detach -p 5432:5432 -p 5000:5000 -v $WEB_INTERFACE_PATH:/tmp/web -v $TMP_PATH:/tmp/tmp:rw -v $SCRIPT_PATH:/tmp/scripts:ro -v $DB_PATH:/tmp/DB:ro web2epub
+echo "Docker exec"
+sleep 5
+docker exec web2epub$CONT_ITER /bin/sh -c '/tmp/DB/restore.ps'
+docker exec web2epub$CONT_ITER /bin/sh -c 'cd /tmp/web && ./flask.sh &'
--- run.sh
+++
@@ -1,3 +0,0 @@
-docker build -t web2epub . -f Dockerfile
-docker rm -f web2epub
-docker run --rm --name web2epub -p 5000:5000 web2epub
