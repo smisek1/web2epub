d0a7e503b9690dc981b25f5dc033606888ba27b7
rpi <tsmisek@seznam.cz>
2023-02-17 22:10:01 +0000

    changed backup

--- backup/restore.sh
+++ backup/restore.sh
@@ -2,7 +2,7 @@ BACKUP_DIR="/home/pi/Documents/NAS/_filmy/_ostatni/conversion/regulary/"
 
 # Get the name of the latest backup file
 LATEST_BACKUP_FILE=$(ls -1t $BACKUP_DIR | head -n 1)
-
+#LATEST_BACKUP_FILE="2023-02-14.tar"
 # Restore the database from the backup file
 export PGPASSWORD='Pa$$w0rd' 
 psql -U postgres -h localhost -p 5432 -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE pg_stat_activity.datname = 'conversion';"
